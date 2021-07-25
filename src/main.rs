863b6c4f02e35667485c0e81bdf5eac7653e1e19
Pavel Sobolev <paveloom@tuta.io>
2021-07-25 13:17:35 +0300

    Test the resizing bug.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use fltk::{app, button::Button, prelude::*, window::Window};
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut w = Window::new(100, 100, 250, 250, "");
+
+    let bw = 60;
+    let bh = 30;
+    let mut b = Button::default()
+        .with_pos((w.w() - bw) / 2, (w.h() - bh) / 2)
+        .with_size(bw, bh)
+        .with_label("Resize");
+    b.set_callback(resize);
+
+    w.show();
+    app.run().unwrap();
+}
+
+fn resize(b: &mut Button) {
+    if let Some(ref mut w) = b.parent() {
+        if w.h() == 250 {
+            w.resize(w.x(), w.y(), w.w(), 150);
+        } else {
+            w.resize(w.x(), w.y(), w.w(), 250);
+        }
+        b.set_pos((w.w() - b.w()) / 2, (w.h() - b.h()) / 2);
+    }
 }
