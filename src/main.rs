a643c136e85e188cfd63077aa16eabcecc8bc835
Pavel Sobolev <paveloom@tuta.io>
2021-07-25 16:40:33 +0300

    Add some free-for-resizing height.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let ww = 250;
     let wh = 250;
     let mut w = Window::new(100, 100, ww, wh, "");
-    w.size_range(ww, wh, ww, wh);
+    w.size_range(ww, wh, ww, wh + 100);
 
     let gh = 40;
     let mut bg = Group::default().with_pos(10, 10).with_size(w.w() - 20, gh);
@@ -29,6 +29,7 @@ fn main() {
         .with_size(w.w() - 20, w.h() - 10 - gh - 10 - 10);
     f.set_frame(FrameType::BorderBox);
 
+    w.resizable(&f);
     w.show();
     app.run().unwrap();
 }
@@ -37,14 +38,15 @@ fn resize(b: &mut Button) {
     if let Some(ref g) = b.parent() {
         if let Some(ref mut w) = g.parent() {
             if let Some(ref mut s) = w.child(1) {
-                if w.h() == 250 {
-                    w.resize(w.x(), w.y(), w.w(), 10 + g.h() + 10);
-                    b.set_label("Show");
-                    s.hide();
-                } else {
+                let gh = 10 + g.h() + 10;
+                if w.h() == gh {
                     w.resize(w.x(), w.y(), w.w(), 250);
                     b.set_label("Hide");
                     s.show();
+                } else {
+                    w.resize(w.x(), w.y(), w.w(), gh);
+                    b.set_label("Show");
+                    s.hide();
                 }
             }
         }
