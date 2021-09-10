f1e612f147236617daeced14422afd45e4e973b3
Pavel Sobolev <paveloom@tuta.io>
2021-09-10 09:45:31 +0300

    Add the code of the test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use fltk::{app, group::Group, enums::FrameType, button::Button, prelude::*, window::Window};
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+
+    app::background(255, 255, 255);
+    app::set_frame_type(FrameType::FlatBox);
+
+    let mut w = Window::new(100, 100, 400, 300, "Focus Box Test");
+
+    let g = Group::default().with_size(180, 190).center_of(&w);
+
+    let h = 40;
+
+    let _b1 = Button::new(g.x(), g.y(), 60, h, "60");
+    let _b2 = Button::new(g.x(), g.y() + h + 10, 100, h, "100");
+    let _b3 = Button::new(g.x(), g.y() + 2 * h + 10, 140, h, "140");
+    let _b4 = Button::new(g.x(), g.y() + 3 * h + 10, 180, h, "180");
+
+    g.end();
+
+    w.end();
+    w.show();
+    app.run().unwrap();
 }
